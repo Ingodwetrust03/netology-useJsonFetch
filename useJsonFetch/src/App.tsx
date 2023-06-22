3b88627960a2d7ab2fa87437a3a4535e228a76ae
svetlana18055 <svetlana18055@yandex.ru>
2023-06-22 20:55:16 +0300

    useJsonFetch

---
+++ useJsonFetch/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import useJsonFetch from "./hooks/useJsonFetch";
+
+function App() {
+    // http://localhost:7070/data
+    // http://localhost:7070/loading
+    // http://localhost:7070/error
+
+
+    const API_URL = 'http://localhost:7070/data'
+    const[{data: response, loading, error}] = useJsonFetch(API_URL, [])
+
+
+    return (
+    <>
+        {loading ? <p>loading...</p> : ''}
+        <p>Status: {error ? error : response.status}</p>
+    </>
+  )
+}
+
+export default App
---
+++ useJsonFetch/src/hooks/useJsonFetch.tsx
@@ -0,0 +1,54 @@
+import {useEffect, useState} from "react";
+
+const useJsonFetch = (url, opts) => {
+    const[data, setData] = useState(opts)
+    const[loading, setLoading] = useState(false)
+    const[error, setError] = useState(false)
+
+
+    useEffect( () => {
+            let controller
+
+            const stop = () => {
+                if (controller) {
+                    controller.abort()
+                }
+            }
+
+            const fetchData = async () => {
+
+                try {
+                    stop()
+
+                    controller = new AbortController();
+                    setLoading(true)
+
+                    const response = await fetch(url, controller.signal)
+                    if(!response.ok) {
+                        throw new Error ("Ошибка при передаче данных")
+                    }
+                    const json = await response.json()
+                    setData(json)
+                } catch (error) {
+                    setError(error.message)
+                } finally {
+                    setLoading(false)
+                }
+            }
+
+
+            fetchData();
+
+            return () => {
+                stop()
+            }
+
+
+        }, [url])
+
+
+    return [{data, loading, error}]
+
+}
+
+export default useJsonFetch
